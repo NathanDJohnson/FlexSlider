410ac8d6b6cae73058e509d63a68ef092673bf93
NathanDJohnson <nathanj.runs@gmail.com>
2015-06-12 10:05:56 -0600

    make sure classipress is the theme

--- flexslider.php
+++ flexslider.php
@@ -22,16 +22,18 @@ function cpflex_theme_exists() {
 }
 
 function cpflex_flexslider_enqueue() {
-	if( is_home() || is_front_page() || is_tax( array( APP_TAX_CAT, APP_TAX_TAG ) ) ) {
+	if ( cpflex_theme_exists() ) {
+		if( is_home() || is_front_page() || is_tax( array( APP_TAX_CAT, APP_TAX_TAG ) ) ) {
 
-		// Deregister jquery to avoid conflicts with jquery hosted on googleapis.com
-		wp_deregister_script('jquery');
+			// Deregister jquery to avoid conflicts with jquery hosted on googleapis.com
+			wp_deregister_script('jquery');
 
-		wp_enqueue_style( 'flexslider-css', plugin_dir_url( __FILE__ ) . 'css/flexslider.css' );
-		wp_enqueue_style( 'classipress-slider-css', plugin_dir_url( __FILE__ ) . 'css/demo.css' );
+			wp_enqueue_style( 'flexslider-css', plugin_dir_url( __FILE__ ) . 'css/flexslider.css' );
+			wp_enqueue_style( 'classipress-slider-css', plugin_dir_url( __FILE__ ) . 'css/demo.css' );
 
-		wp_enqueue_script( 'g-jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js', array() );
-		wp_enqueue_script( 'flexslider-js', plugin_dir_url( __FILE__ ) . 'js/jquery.flexslider.js', array( 'g-jquery' ) );
+			wp_enqueue_script( 'g-jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js', array() );
+			wp_enqueue_script( 'flexslider-js', plugin_dir_url( __FILE__ ) . 'js/jquery.flexslider.js', array( 'g-jquery' ) );
+		}
 	}
 }
 add_action( 'wp_enqueue_scripts', 'cpflex_flexslider_enqueue' );
